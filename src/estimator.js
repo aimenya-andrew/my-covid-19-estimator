16475ff265acc32b98ce1d418af8e9a850df109d
aimenya-andrew <ehikioyaandrew@gmail.com>
2020-04-07 21:07:11 +0100

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,17 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input,
+        impact: {
+            currentlyInfected: reportedCases * 10,
+            infectionsByRequestedTime: currentlyInfected * 512,
+
+        },
+        severeImpact: {
+            currentlyInfected: reportedCases * 50,
+            infectionsByRequestedTime: currentlyInfected * 512,
+        }
+    };
+};
 
 export default covid19ImpactEstimator;
