f3b4d2d55cd8dac2bc61c2665a97ac936a823d2f
aimenya-andrew <ehikioyaandrew@gmail.com>
2020-04-08 15:39:09 +0100

    Challenge 1k

--- src/estimator.js
+++ src/estimator.js
@@ -5,9 +5,9 @@ const covid19ImpactEstimator = (data) => {
   const severeImpact = {};
   if ((data.periodType) === 'days') {
     factor = Math.floor((time / 3));
-  } else if ((data.timeToElapse) === 'weeks') {
+  } else if ((data.periodType) === 'weeks') {
     factor = Math.floor(((time / 3) * 7));
-  } else if (((data.timeToElapse) === 'months')) {
+  } else if (((data.periodType) === 'months')) {
     factor = Math.floor(((time / 3) * 30));
   }
   impact.currentlyInfected = ((data.reportedCases) * 10);
